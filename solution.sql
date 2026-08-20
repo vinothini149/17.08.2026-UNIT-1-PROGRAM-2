995c86daaf15828e36277b70ad53a4dd32ccab61
vinothini149 <vinothiniav1409@gmail.com>
2026-08-20 14:09:25 +0530

    PROGRAM 2

--- solution.sql
+++ solution.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE 1U25IT115DB;
+USE 1U25IT115DB;
+CREATE TABLE Department(
+  StudentID VARCHAR(5)PRIMARY KEY,
+  StudentName VARCHAR(20)NOT NULL,
+  DOB Date,
+  Gender VARCHAR(10),
+  DepartmentID VARCHAR(5)NOT NULL
+);
+desc Department;
+ALTER TABLE department
+ADD(
+ Email VARCHAR(30),
+ PhoneNumber Numeric(10)
+ );
+ desc Department;
 
